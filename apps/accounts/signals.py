0651068f729c3388bd2e6fa957d058442973d111
7amzey <hamze.obaid2004@gmail.com>
2026-02-27 03:36:23 +0300

    update signals.py file

--- apps/accounts/signals.py
+++ apps/accounts/signals.py
@@ -1,13 +1,14 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import User
+from apps.students.models import Student
+from apps.instructors.models import Instructor
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         if instance.role == 'student':
-            from apps.students.models import Student
             Student.objects.create(user=instance)
         elif instance.role == 'instructor':
-            from apps.instructors.models import Instructor
             Instructor.objects.create(user=instance)
